57d880f1d9daa23ecab165fc30c5cd95a8c0af37
Haosik <noobbaster@gmail.com>
2019-05-15 20:00:39 +0300

    done Mutations.

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -17,7 +17,7 @@ class LaunchAPI extends RESTDataSource {
   }
 
   async getLaunchesByIds({ launchIds }) {
-    return Promise.all(launchIds.map(launchId => getLaunchById({ launchId })));
+    return Promise.all(launchIds.map(launchId => this.getLaunchById({ launchId })));
   }
 
   launchReducer(launch) {
